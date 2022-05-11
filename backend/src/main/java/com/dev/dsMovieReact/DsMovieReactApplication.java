56b42026ab5cb75659d5f98475be95807dda8b87
Joas Choque <61289634+JoasChoque@users.noreply.github.com>
2022-05-11 15:30:44 -0300

    Starting Project

---
+++ backend/src/main/java/com/dev/dsMovieReact/DsMovieReactApplication.java
@@ -0,0 +1,13 @@
+package com.dev.dsMovieReact;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsMovieReactApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DsMovieReactApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/dev/dsMovieReact/DsMovieReactApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dev.dsMovieReact;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsMovieReactApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
